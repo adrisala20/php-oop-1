097033c8334571b9239912be616ee572b4447ee6
adrisala20 <adriana_15_tuya@hotmail.com>
2024-05-07 15:19:55 +0200

    added structure

---
+++ View/footer.php
@@ -0,0 +1,12 @@
+<?php
+
+?>
+<footer>
+    <div class="container">
+        <p class="text-center">
+        Made with &hearts; by AD
+        </p>
+    </div>
+</footer>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+include __DIR__ ."/View/header.php";
+?>
+
+<main>
+
+</main>
+
+<?php 
+include __DIR__ ."/View/footer.php";
+?>
